045b910c8d03f246d6d7ca2936f728fbbefe5cb1
Tomasz Zboralski <tomasz.zboralski@gmail.com>
2021-02-25 19:57:35 +0100

    ProductController changes missed in last commit added now

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,4 +1,40 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.ProductDto;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/product")
 public class ProductController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "getProducts")
+    public List<ProductDto> getProducts() {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getProduct")
+    public ProductDto getProduct(Long id) {
+        return new ProductDto();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createProduct")
+    public ProductDto createProduct() {
+        return new ProductDto();
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateProduct")
+    public ProductDto updateProduct(ProductDto productDto) {
+        return new ProductDto();
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct")
+    public void deleteProduct(Long id) {
+
+    }
+
 }
