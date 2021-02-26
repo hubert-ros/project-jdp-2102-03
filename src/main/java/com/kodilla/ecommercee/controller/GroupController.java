f633f9b698ff48a572a48c23c3011884fdc9e90f
Jola-Witaszak <jola-w@o2.pl>
2021-02-26 21:23:10 +0100

    "GroupController - initial implementation of a class returning sample data"

---
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -0,0 +1,34 @@
+package com.kodilla.ecommercee.controller;
+
+import com.kodilla.ecommercee.domain.Group;
+import com.kodilla.ecommercee.domain.dto.GroupDto;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/v1/group")
+@CrossOrigin(origins = "*")
+public class GroupController {
+
+    @GetMapping(value = "getGroups")
+    public List<Group> getGroups() {
+        return new ArrayList<>();
+    }
+
+    @PostMapping(value = "createGroup", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createGroup(@RequestBody GroupDto groupDto) {
+    }
+
+    @GetMapping(value = "getGroup")
+    public GroupDto getGroup(@RequestParam long groupId) {
+        return new GroupDto(1,"this is only one group");
+    }
+
+    @PutMapping(value = "updateGroup", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public GroupDto updateGroup(@RequestBody GroupDto groupDto) {
+        return new GroupDto(1, "The best updated group!");
+    }
+}
