5cc9130d17282f2a7b98ebc832198ff64ff4510c
Jola-Witaszak <jola-w@o2.pl>
2021-02-26 21:23:10 +0100

    "GroupController - initial implementation of a class returning sample data"

--- src/main/java/com/kodilla/ecommercee/EcommerceeApplication.java
+++ src/main/java/com/kodilla/ecommercee/EcommerceeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EcommerceeApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(EcommerceeApplication.class, args);
     }
