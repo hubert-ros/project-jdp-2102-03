82ae85f0afb8ac374c80593868d062cad16f4f50
Paweł Praga <p.praga@o2.pl>
2021-02-27 14:34:36 +0100

    JDP210203-5 new chenges in UserController

--- src/main/java/com/kodilla/ecommercee/controller/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/controller/ProductController.java
@@ -1,41 +1,5 @@
 package com.kodilla.ecommercee.controller;
 
-import com.kodilla.ecommercee.domain.ProductDto;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@RestController
-@RequestMapping("/v1/product")
 public class ProductController {
-
-    @RequestMapping(method = RequestMethod.GET, value = "getProducts")
-    public List<GenericEntity> getProducts() {
-        return new ArrayList<>();
-    }
-
-    @RequestMapping(method = RequestMethod.GET, value = "getProduct")
-    public GenericEntity getProduct(@PathVariable Long id) {
-        return new GenericEntity("1");
-    }
-
-    @RequestMapping(method = RequestMethod.POST, value = "createProduct")
-    public GenericEntity createProduct() {
-        return new GenericEntity("2");
-    }
-
-    @RequestMapping(method = RequestMethod.PUT, value = "updateProduct")
-    public GenericEntity updateProduct(ProductDto productDto) {
-        return new GenericEntity("3");
-    }
-
-    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct")
-    public void deleteProduct(@PathVariable Long id) {
-
-    }
 
 }
