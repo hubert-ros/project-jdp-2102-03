05e684705759edd33c094b578ac637ac770722a4
Paweł Praga <p.praga@o2.pl>
2021-02-27 13:01:50 +0100

    JDP210203-5 new chenges in UserController

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -1,4 +1,28 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.GenericEntity;
+import com.kodilla.ecommercee.domain.UserDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/user")
 public class UserController {
+
+    @PostMapping(value = "createUser")
+    public GenericEntity createUser() {
+        return new GenericEntity("user_1");
+    }
+
+    @PutMapping(value = "blockedUser")
+    public GenericEntity blockedUser(Long userId) {
+        return new GenericEntity("user_blocked");
+    }
+
+    @PostMapping(value = "createUserKey")
+    public void createUserKey(Long userKey) {
+
+    }
 }
