02b47e2de8bf24c731bcc833148bea7e5886b709
Jola-Witaszak <jola-w@o2.pl>
2021-03-02 20:50:43 +0100

    Task-JDP210203-2 GroupController - initial implementation returning sample data

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,4 +1,31 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/v1/group")
 public class GroupController {
+
+    @GetMapping(value = "getGroups")
+    public List<GenericEntity> getGroups() {
+        return  new ArrayList<>();
+    }
+
+    @PostMapping(value = "createGroup")
+    public GenericEntity createGroup(GenericEntity genericEntity) {
+        return new GenericEntity("1");
+    }
+
+    @GetMapping(value = "getGroup")
+    public GenericEntity getGroup(long id) {
+        return new GenericEntity("2");
+    }
+
+    @PutMapping(value = "updateGroup")
+    public GenericEntity updateGroup(GenericEntity genericEntity) {
+        return new GenericEntity("3");
+    }
 }
