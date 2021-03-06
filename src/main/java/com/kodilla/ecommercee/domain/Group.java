13a2bb999dbefde9922e1cced06b36268ea0b059
Arkodiusz <arkadiusz.jedrzej@gmail.com>
2021-03-06 20:58:18 +0100

    Group entity created

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -1,4 +1,29 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name = "ENTITY_GROUP")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Group {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String name;
+
+    @OneToMany(
+            mappedBy = "groupOfProduct"
+    )
+    private List<Product> products;
 }
