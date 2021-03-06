e05b650cf49def65ab0bb942a47a2af5d5aad0ad
Arkodiusz <arkadiusz.jedrzej@gmail.com>
2021-03-06 21:04:12 +0100

    lombok added to build.gradle and mock Product class created

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,2 +1,24 @@
-package com.kodilla.ecommercee.domain;public class Product {
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity(name = "ENTITY_PRODUCT")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Long ID;
+
+    @ManyToOne
+    @JoinColumn(name = "GROUP_ID")
+    private Group groupOfProduct;
 }
