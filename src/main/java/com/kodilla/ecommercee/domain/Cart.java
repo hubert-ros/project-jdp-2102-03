89d9465bed33dbfca9ce64cbbca1d072736247d8
Paweł Praga <p.praga@o2.pl>
2021-03-07 17:45:02 +0100

    JDP210203-14 create a Cart entity

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -1,4 +1,32 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "CARTS")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Cart {
+
+        @Id
+        @NotNull
+        @GeneratedValue
+        @Column(name = "CART_ID", unique = true)
+        private Long cartId;
+
+        @OneToMany(
+                targetEntity = Product.class,
+                mappedBy = "carts",
+                cascade = CascadeType.ALL,
+                fetch = FetchType.LAZY
+        )
+        public List<Product> productsInCart = new ArrayList<>();
 }
+
+
--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,2 +1,28 @@
-package com.kodilla.ecommercee.domain;public class Product {
+package com.kodilla.ecommercee.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Entity(name = "PRODUCTS")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @NotNull
+    @GeneratedValue
+    @Column(name = "PRODUCT_ID", unique = true)
+    private Long productId;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "PRODUCTS_IN_CART")
+    private Cart carts;
 }
