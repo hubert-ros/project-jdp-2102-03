3c9e5d12cc5eceaf33750164040ddbcb7513aa2a
michkubik <michal.kubik@o2.pl>
2021-03-08 00:28:28 +0100

    Task JDP210203-12 Product branch created

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,2 +1,25 @@
-package com.kodilla.ecommercee.domain;public class Product {
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity(name = "ENTITY_PRODUCT")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ID;
+
+    @ManyToMany(mappedBy = "products")
+    private Set<Group> groupsOfProduct = new HashSet<>();
 }
