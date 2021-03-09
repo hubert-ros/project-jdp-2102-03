39bbae631cca433de96e4c557e1630c12ee17b39
Jola-Witaszak <jola-w@o2.pl>
2021-03-09 17:43:52 +0100

    Task-JDP210203-15 "Added User entity, UserRepository (and sample Order entity)"

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -1,4 +1,36 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "USERS")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private String e_mail;
+
+    @NotNull
+    private String address;
+
+    @OneToMany(mappedBy = "user",
+            fetch = FetchType.EAGER,
+            cascade = CascadeType.ALL)
+    private final Set<Order> orders = new HashSet<>();
 }
