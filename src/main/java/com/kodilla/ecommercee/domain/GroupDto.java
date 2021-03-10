687a8b28bb0fe2ef76ab87610e0da77108e2d502
Arkodiusz <arkadiusz.jedrzej@gmail.com>
2021-03-10 21:56:36 +0100

    jdp210203-13 groupDto removed

--- src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
@@ -1,14 +1,4 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-import java.util.Set;
-
-@Getter
-@AllArgsConstructor
 public class GroupDto {
-    private Long id;
-    private String name;
-    private Set<Product> products;
 }
