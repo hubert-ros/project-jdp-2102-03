b9fbc70df2bb6a85e7462e0fb8d060bf54f856b7
Paweł Praga <p.praga@o2.pl>
2021-03-12 16:25:15 +0100

    JDP210203-14 changes in Cart entity

---
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -0,0 +1,23 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ORDERS")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+
+}
