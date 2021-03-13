0a241224c1e1d13bdd9f58fd82a12355813a7a3e
Paweł Praga <p.praga@o2.pl>
2021-03-13 10:47:44 +0100

    JDP210203-14 Cart entity

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -3,7 +3,6 @@ package com.kodilla.ecommercee.domain;
 import lombok.*;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
