6e87670ac473c029bdd48297328aba00d5c9c44c
michkubik <michal.kubik@o2.pl>
2021-03-17 16:26:18 +0100

    Task JDP210203-17 Work started

---
+++ src/test/java/com/kodilla/ecommercee/repository/ProductTestSuite.java
@@ -0,0 +1,53 @@
+package com.kodilla.ecommercee.repository;
+
+import com.kodilla.ecommercee.domain.Cart;
+import com.kodilla.ecommercee.domain.Order;
+import com.kodilla.ecommercee.domain.Product;
+import com.kodilla.ecommercee.domain.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+
+public class ProductTestSuite {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+
+    @Test
+    public void testProductAddedToCart() {
+
+        //Given
+        Product product1 = new Product(new String("Shoes"), new String("Running shoes"), new BigDecimal(199.00));
+
+
+        //When
+
+        //Then
+
+    }
+
+
+
+}
