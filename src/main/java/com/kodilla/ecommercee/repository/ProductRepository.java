3eb322ece3cca9ef1555ecafe55c0c29eaad74d4
michkubik <michal.kubik@o2.pl>
2021-03-20 23:33:41 +0100

    Task JDP210203-17 PRODUCT TESTS PO UWAGACH 3

--- src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @Transactional
@@ -14,7 +15,7 @@ public interface ProductRepository extends CrudRepository<Product, Long> {
 
     List<Product> findByProductName(String name);
 
-    List<Product> findByProductId(Long id);
+    Optional<Product> findByProductId(Long id);
 
 
     @Override
